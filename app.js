4cb5758f5510a5cff3876e80b296b10fdee56a20
Nikita Kuznetsov <livisback@yandex.ru>
2022-12-19 21:59:41 +0600

    add app js

---
+++ app.js
@@ -0,0 +1,13 @@
+const cols = document.querySelectorAll('.col')
+
+function generateRandomColor () {
+    
+}
+
+function setRandomColors() {
+    cols.forEach((col) => {
+        console.log(col)
+    })
+}
+
+setRandomColors()
