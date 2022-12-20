b892687b3ad02b9efb06037f5d1d99269a5e23f7
Nikita Kuznetsov <livisback@yandex.ru>
2022-12-20 09:48:02 +0600

    add func setTextColor

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ function setRandomColors() {
 
         text.textContent = color
         col.style.background = color
+
+        setTextColor(text, color)
     })
 }
 
+function setTextColor(text, color) {
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance > 0.5 ? 'black' : 'white'
+}
 setRandomColors()
